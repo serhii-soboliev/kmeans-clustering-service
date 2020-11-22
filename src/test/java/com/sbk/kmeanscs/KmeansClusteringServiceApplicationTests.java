2d47b9eec0b1270111d2c7f7a54c8da866c84145
Serhii Soboliev <sergiy.sobolev@gmail.com>
2020-11-22 15:08:27 +0200

    init commit

---
+++ src/test/java/com/sbk/kmeanscs/KmeansClusteringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbk.kmeanscs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmeansClusteringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
