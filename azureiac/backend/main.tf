fb0f1b3a6d1088f310f5af744520795372db96f6
Serhii Soboliev <sergiy.sobolev@gmail.com>
2020-12-06 00:46:47 +0200

    Terraform create container registry

--- azureiac/backend/main.tf
+++ azureiac/backend/main.tf
@@ -3,6 +3,13 @@ resource "azurerm_resource_group" "kmeans_cs_rg" {
   location = "West Europe"
 }
 
+resource "azurerm_container_registry" "kmeans_cr_container_registry" {
+  name                     = "kmeanscr"
+  resource_group_name      = azurerm_resource_group.kmeans_cs_rg.name
+  location                 = "West Europe"
+  sku                      = "Basic"
+}
+
 resource "azurerm_virtual_network" "kmeans_cs_network" {
   name = "kmeans_cs_network"
   address_space = ["10.0.0.0/16"]
